413713a26e21341d0a0b3c971ed60d67d364a080
beardyman <therealbeardyman@gmail.com>
2016-12-02 21:43:59 -0500

    added tests for rules

--- test/unit/model/deck.spec.js
+++ test/unit/model/deck.spec.js
@@ -1,3 +1,21 @@
 /**
  * Created by jnornhold on 11/30/16.
  */
+"use strict";
+
+describe('Deal Card', () => {
+  require('test/testUtils/suiteBootstrap')(global);
+
+  let Deck = require('model/deck')
+    , deck;
+
+  beforeEach(() => {
+    deck = new Deck(1);
+  });
+
+  it('should deal a 1', () => {
+    expect(deck.deal().value).to.equal(1);
+    expect(deck.cards.length).to.equal(6);
+  });
+
+});
--- test/unit/model/game.spec.js
+++ test/unit/model/game.spec.js
@@ -1,3 +1,9 @@
 /**
  * Created by jnornhold on 11/30/16.
  */
+"use strict";
+
+describe('Game', () => {
+  require('test/testUtils/suiteBootstrap')(global);
+
+});
